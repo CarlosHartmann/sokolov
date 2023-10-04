8ba63f071bfb195509f32ed7c1f5f7feb1b8a8ed
Smogshaik <github@smog.sh>
2023-10-04 13:37:57 +0200

    initialized project w/ poetry

---
+++ sokolov/main.py
@@ -0,0 +1,15 @@
+'''
+sokolov: An LLM-experimenting suite for my singular 'they' research. Will include data migration, cleanup, and checking features.
+
+sokolov sokolov sokolov!
+sokolov?
+That's right.
+'''
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
